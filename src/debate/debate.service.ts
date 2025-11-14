7124a072c8cd4164daf5b52acae150cbad60de7f
bitbyte08 <me@bitworkspace.kr>
2025-11-14 18:49:29 +0900

    feat :: debate Service

--- src/debate/debate.service.ts
+++ src/debate/debate.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { v4 } from 'uuid';
 
+interface DebateRoom {
+  name: string; // RoomName
+  participants: string[]; // UserId Array
+}
 @Injectable()
-export class DebateService {}
+export class DebateService {
+  private debateRooms: Record<string, DebateRoom> = {}; // Rooms InMemory Record
+
+  createDebateRoom(name: string, createUserId: string): DebateRoom {
+    let roomId: string;
+    do {
+      roomId = v4();
+    } while (this.debateRooms[roomId]);
+
+    this.debateRooms[roomId] = { name: name, participants: [createUserId] };
+    return this.debateRooms[roomId];
+  }
+
+  getDebateRooms(): DebateRoom[] {
+    return Object.values(this.debateRooms);
+  }
+}
