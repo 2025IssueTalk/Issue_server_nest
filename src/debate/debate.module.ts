b9634954be1a825b7501e86af9dd3e83a8773704
bitbyte08 <me@bitworkspace.kr>
2025-11-14 18:35:01 +0900

    feat :: 토론 방 관련 REST API 추가

--- src/debate/debate.module.ts
+++ src/debate/debate.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DebateGateway } from './debate.gateway';
+import { DebateController } from './debate.controller';
+import { DebateService } from './debate.service';
 
 @Module({
-  providers: [DebateGateway]
+  providers: [DebateGateway, DebateService],
+  controllers: [DebateController]
 })
 export class DebateModule {}
