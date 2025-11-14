7c97fcc06d9a5e2b1ec4435d6f19c0a77f19fad0
bitbyte08 <me@bitworkspace.kr>
2025-11-14 19:27:57 +0900

    feat :: Debate 생성 API 구현

--- src/debate/debate.controller.ts
+++ src/debate/debate.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { DebateService } from './debate.service';
+import {
+  CreateDebateRequestDto,
+  CreateDebateResponseDto,
+} from './dto/create-debate.dto';
+import { User } from '../common/decorators/user.decorator';
 
 @Controller('debate')
-export class DebateController {}
+export class DebateController {
+  constructor(private readonly debateService: DebateService) {}
+
+  @Post()
+  createDebate(
+    @Body() body: CreateDebateRequestDto,
+    @User('userId') userId: string,
+  ): CreateDebateResponseDto {
+    return this.debateService.createDebateRoom(body.name, userId);
+  }
+}
